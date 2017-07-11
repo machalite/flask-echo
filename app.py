0d7af69c937dfbefae5c6bae6ec462c9591b62de
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-07-12 00:50:58 +0700

    secret and channel token correction

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', 'a62c4f246f3dfe799fa69c44d9d99a82')
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', 'DUTE1UOjEqCQpJynGDa59KSV42WXmVjM8/Dw2qaFuyA9ePaA40Qy2lHRcfRaM0SzM3HpvNYySB2IrkJGiQ+1RktH1Ko6285vipalBZ8WtDy+6T1pRZDnS/NHDvUgadaxLCR0TbACjTKRyZkMpOjYUgdB04t89/1O/w1cDnyilFU=')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', a62c4f246f3dfe799fa69c44d9d99a82)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', DUTE1UOjEqCQpJynGDa59KSV42WXmVjM8/Dw2qaFuyA9ePaA40Qy2lHRcfRaM0SzM3HpvNYySB2IrkJGiQ+1RktH1Ko6285vipalBZ8WtDy+6T1pRZDnS/NHDvUgadaxLCR0TbACjTKRyZkMpOjYUgdB04t89/1O/w1cDnyilFU=)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
